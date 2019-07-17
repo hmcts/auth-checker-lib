9ef39ef47c91276769f08c6c74f36c74d0d152b5
Attila Kiss <attila.kiss@hmcts.net>
2019-07-17 12:37:07 +0100

    [AK] Update dependencies and fix issues related to the new libraries

--- src/main/java/uk/gov/hmcts/reform/auth/parser/idam/core/user/token/UserTokenDetails.java
+++ src/main/java/uk/gov/hmcts/reform/auth/parser/idam/core/user/token/UserTokenDetails.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.auth.parser.idam.core.user.token;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Set;
@@ -10,4 +12,10 @@ import java.util.Set;
 public class UserTokenDetails {
     private final String id;
     private final Set<String> roles;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public UserTokenDetails(@JsonProperty("id") String id, @JsonProperty("roles")Set<String> roles) {
+        this.id = id;
+        this.roles = roles;
+    }
 }
