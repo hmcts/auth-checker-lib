62bca36257fe3a11a951b08a4ec1ed8de67dce39
Michael1142 <michael.johnson9@justice.gov.uk>
2024-06-17 16:01:44 +0100

    Explicitly name @Bean and Remove @ConditionalOnMissingBean and @Qualifier

--- src/main/java/uk/gov/hmcts/reform/auth/parser/idam/spring/user/token/UserTokenParserConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/auth/parser/idam/spring/user/token/UserTokenParserConfiguration.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.reform.auth.parser.idam.spring.user.token;
 
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -16,15 +14,14 @@ import uk.gov.hmcts.reform.auth.parser.idam.core.user.token.UserTokenParser;
 @Configuration
 public class UserTokenParserConfiguration {
 
-    @Bean
-    @ConditionalOnMissingBean(name = "userTokenParserHttpClient")
+    @Bean("userTokenParserHttpClient")
     public CloseableHttpClient userTokenParserHttpClient() {
         return HttpClients.createDefault();
     }
 
     @Bean
     public UserTokenParser<UserTokenDetails> userTokenParser(
-        @Qualifier("userTokenParserHttpClient") CloseableHttpClient userTokenParserHttpClient,
+        CloseableHttpClient userTokenParserHttpClient,
         @Value("${auth.idam.client.baseUrl}") String baseUrl) {
             return new HttpComponentsBasedUserTokenParser<>(userTokenParserHttpClient, baseUrl, UserTokenDetails.class);
     }
